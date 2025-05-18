4d53f0650a23c76026d6cf47f04277621e10a728
Sergio Jr. Barajas Montes <sergiom@cetys.edu.mx>
2025-05-18 14:50:50 -0700

    Update sfml_bosquejo.cpp

--- sfml_bosquejo.cpp
+++ sfml_bosquejo.cpp
@@ -8,6 +8,8 @@ int main()
 {
  
 RenderWindow window(VideoMode({1920, 1080}), "---LOGIC---");
+
+
         //cargar fuente
         Font fuente1("C:/Users/Sergio/Desktop/SFML-3.0.0/Nabla.ttf");
         Font fuente2("C:/Users/Sergio/Desktop/SFML-3.0.0/Jersey.ttf");
@@ -44,8 +46,14 @@ RenderWindow window(VideoMode({1920, 1080}), "---LOGIC---");
 
         //texto
         Text texto1(fuente1, "LOGIC", 200);
+        texto1.setOutlineThickness(5);
+        texto1.setOutlineColor(Color::Black);
         Text texto2(fuente2, "INICIO", 50);
+         texto2.setOutlineThickness(5);
+        texto2.setOutlineColor(Color::Black);
         Text texto3(fuente2, "SALIR", 50);
+         texto3.setOutlineThickness(5);
+        texto3.setOutlineColor(Color::Black);
         
         //loop del programa
         while(window.isOpen())
@@ -86,32 +94,135 @@ RenderWindow window(VideoMode({1920, 1080}), "---LOGIC---");
             //dibujar donde esta el mouse
             mouse.setPosition(sf::Vector2f(posicionMouse));
             mouse.setOrigin(mouse.getGeometricCenter());
-            //estado del mouse
+           
+            
+               // state of button
+             
+            bool botonPresionado = false;
+
+             // state of mouse
+            bool mousePresionado = false;
+              // state of mouse
+            bool onMousePress   = false;
+            bool onMouseRelease = false;
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        {
+            if (not mousePresionado)
+                onMousePress = true;
+            mousePresionado = true;
+        }
+        else
+        {
+            if (mousePresionado)
+                onMouseRelease = true;
+            mousePresionado = false;
+        }
+
 
             // interaccion del mouse con los botones
-            
-            if (rectanguloInicio1.getGlobalBounds().contains(posicionMouse)){
+            // BOTON INICIO
+            if (rectanguloInicio1.getGlobalBounds().contains(posicionMouse))
+            {
                 rectanguloInicio1.setFillColor(Color(102,204,0));
                 rectanguloInicio1.setOutlineColor(Color(76,153,0));
+                texto2.setOutlineColor(Color::White);
                 texto2.setFillColor(Color::Black);
-               
+                    if (onMousePress)
+                        botonPresionado = true;
+                
+                    if (mousePresionado and botonPresionado)
+                        {
+                        rectanguloInicio1.setFillColor(sf::Color::Yellow);
+                        window.clear(sf::Color(0,51,25));
+                        window.display();
+                        //aqui cambiar de pantalla
+                       window.close();
 
+                        RenderWindow window2(VideoMode({1980,1060}), "---LOGIC---");
+                        while(window2.isOpen())
+                       {
+
+                                    while(std::optional event = window2.pollEvent())
+                                    {
+                                        if(event->is<sf::Event::Closed>())
+                                        {
+                                            window2.close();
+                                        }
+                                        else if(event->is<sf::Event::Resized>())
+                                        {
+                                        sf::View vista(sf::FloatRect({0.f, 0.f}, sf::Vector2f(window.getSize())));
+                                            window2.setView(vista);
+                                        }
+
+                                    }
+                                        window2.clear(Color(0,51,25));
+                                        
+                                        Text seleccion(fuente2, "Seleccione una opcion: ", 70);
+                                        RectangleShape rectangulo({anchoRC, altoRC});
+                                        RectangleShape rectanguloInicio1({anchoRI1, altoRI1});
+                                        RectangleShape rectanguloInicio2({anchoRI2, altoRI2});
+
+                                        seleccion.setPosition({ventana_ancho/3, ventana_alto/8});
+                                        window2.draw(seleccion);
+                                        window2.display();
+
+
+
+
+                    
+                            
+                        }
+                                        
+                    }
+                    else
+                        rectanguloInicio1.setFillColor(sf::Color(102, 204, 0));
             }else{
+
                 rectanguloInicio1.setFillColor(Color(76,153,0));
                 rectanguloInicio1.setOutlineColor(Color(102,204,0));
                 texto2.setFillColor(Color::White);
+                texto2.setOutlineColor(Color::Black);
+                rectanguloInicio1.setFillColor(sf::Color(102,204,0));
+           
             }
+            if (not mousePresionado)
+            botonPresionado = false;
 
-            if (rectanguloInicio2.getGlobalBounds().contains(posicionMouse)){
-                 rectanguloInicio2.setFillColor(Color(102,204,0));
+
+
+            //BOTON SALIR
+            if (rectanguloInicio2.getGlobalBounds().contains(posicionMouse))
+            {
+                rectanguloInicio2.setFillColor(Color(102,204,0));
                 rectanguloInicio2.setOutlineColor(Color(76,153,0));
+                texto3.setOutlineColor(Color::White);
                 texto3.setFillColor(Color::Black);
+                    if (onMousePress)
+                        botonPresionado = true;
+                
+                    if (mousePresionado and botonPresionado)
+                        {
+                        rectanguloInicio2.setFillColor(sf::Color::Yellow);
+                        window.close();
+                        }
+                    else
+                        rectanguloInicio2.setFillColor(sf::Color(102, 204, 0));
             }else{
+
                 rectanguloInicio2.setFillColor(Color(76,153,0));
                 rectanguloInicio2.setOutlineColor(Color(102,204,0));
                 texto3.setFillColor(Color::White);
+                texto3.setOutlineColor(Color::Black);
+                rectanguloInicio2.setFillColor(sf::Color(102,204,0));
+           
             }
+            if (not mousePresionado)
+            botonPresionado = false;
 
+
+
+          
             //centrar textos
             auto centroRect1x = (rectanguloInicio1.getPosition().x)-(rectanguloInicio1.getSize().x) + 50;
             auto centroRect1y = (rectanguloInicio1.getPosition().y)-(rectanguloInicio1.getSize().y);
@@ -139,6 +250,7 @@ RenderWindow window(VideoMode({1920, 1080}), "---LOGIC---");
             window.display();
 
         }
+        
 
     //fin del programa
     return 0;
